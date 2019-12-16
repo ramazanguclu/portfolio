fb980513e7f0071f6b71d5b749a06122c9199652
ramazanguclu <ramazan.guclu@innovance.com.tr>
2019-12-16 16:23:48 +0300

    added work

--- src/data/Works.js
+++ src/data/Works.js
@@ -1,47 +1,57 @@
 export default [
-    {
-        company: 'Luteg',
-        companyLink: 'http://luteg.com',
-        startDate: 'September 2018',
-        endDate: 'November 2018',
-        position: 'Software Developer',
-        projects: 'Multiportal Devices',
-        usedTech: 'Nodejs(Sailsjs), bootstrap, Less, Html, Css, mongodb'
-    },
-    {
-        company: 'Vestek',
-        companyLink: 'http://vestek.com.tr',
-        startDate: 'June 2017',
-        endDate: 'July 2018',
-        position: 'Software Developer',
-        projects: 'RFHotelTV, MemorialTV',
-        usedTech: 'Javascript, python(django), jQuery, Html, Css, .Net, MSSql, Entity Framework'
-    },
-    {
-        company: 'Insider',
-        companyLink: 'https://useinsider.com',
-        startDate: 'April 2016',
-        endDate: 'June 2017',
-        position: 'Software Developer',
-        projects: 'Custom Works for Our Customers',
-        usedTech: 'Javascript, jQuery, Html, Css, MySql, Php'
-    },
-    {
-        company: 'Sentim',
-        companyLink: 'http://www.sentim.com.tr',
-        startDate: 'July 2014',
-        endDate: 'August 2014',
-        position: 'Intern (Software)',
-        projects: 'akorkupu.com',
-        usedTech: 'Ado.Net, MsSql, Html, Css'
-    },
-    {
-        company: 'Hundai Assan',
-        companyLink: 'http://www.hyundai.com',
-        startDate: 'August 2013',
-        endDate: 'September 2013',
-        position: 'Intern (Hardware)',
-        projects: '-',
-        usedTech: '-'
-    },
-]
+  {
+    company: "Innovance",
+    companyLink: "http://www.innovance.com.tr",
+    startDate: "December 2018",
+    endDate: "-",
+    position: "Software Developer",
+    projects: "Internet Banking Project - Credit Europe Bank Russia",
+    usedTech: "ReactJs, Html, Css, Javascript"
+  },
+  {
+    company: "Luteg",
+    companyLink: "http://luteg.com",
+    startDate: "September 2018",
+    endDate: "November 2018",
+    position: "Software Developer",
+    projects: "Multiportal Devices",
+    usedTech: "Nodejs(Sailsjs), bootstrap, Less, Html, Css, mongodb"
+  },
+  {
+    company: "Vestek",
+    companyLink: "http://vestek.com.tr",
+    startDate: "June 2017",
+    endDate: "July 2018",
+    position: "Software Developer",
+    projects: "RFHotelTV, MemorialTV",
+    usedTech:
+      "Javascript, python(django), jQuery, Html, Css, .Net, MSSql, Entity Framework"
+  },
+  {
+    company: "Insider",
+    companyLink: "https://useinsider.com",
+    startDate: "April 2016",
+    endDate: "June 2017",
+    position: "Software Developer",
+    projects: "Custom Works for Our Customers",
+    usedTech: "Javascript, jQuery, Html, Css, MySql, Php"
+  },
+  {
+    company: "Sentim",
+    companyLink: "http://www.sentim.com.tr",
+    startDate: "July 2014",
+    endDate: "August 2014",
+    position: "Intern (Software)",
+    projects: "akorkupu.com",
+    usedTech: "Ado.Net, MsSql, Html, Css"
+  },
+  {
+    company: "Hundai Assan",
+    companyLink: "http://www.hyundai.com",
+    startDate: "August 2013",
+    endDate: "September 2013",
+    position: "Intern (Hardware)",
+    projects: "-",
+    usedTech: "-"
+  }
+];
